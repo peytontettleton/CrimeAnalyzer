102d32f4fb90bb8626d9b36398b71be1612ba4ef
peytontettleton <36649600+peytontettleton@users.noreply.github.com>
2018-11-02 22:33:17 -0500

    Initialize

---
+++ CrimeAnalyzer/CrimeAnalyzer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrimeAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string csvFilePath = string.Empty;
+            string reportFilePath = string.Empty;
+
+            string startupPath = directory.getcurrentdirectory();
+            
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Call Format: Crime Analyzer <Crime_CSV_File_path> <report_file_path>");
+            }
+        }
+    }
+}
